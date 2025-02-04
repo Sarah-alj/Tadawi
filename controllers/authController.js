641d06f440ed0295be8851ff8f56933911a8ed41
Sarah-alj <saraljurbua@gmail.com>
2025-02-04 20:02:19 +0300

    Updated authController.js path file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
-const User = require("../models/User");
+const User = require('../models/users');  // Updated to match new file name
+
 
 // Signup Controller
 exports.signup = async (req, res) => {
